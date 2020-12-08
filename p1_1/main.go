c32e6cbfc14d3a4e7754b1f696220fb297f9d0bd
Baracat <lbaracat@gmail.com>
2020-12-08 01:04:53 -0300

    Puzzle 1 part 1 done
    Quick and dirt.
    
    Improvements, maybe sorting expense and check if greater than 2020.

---
+++ p1_1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Error opening file. %v", err)
+	}
+	expense, err := getExpense(file)
+	if err != nil {
+		log.Fatalf("Error getting expense. %v", err)
+	}
+
+	for i := 0; i < len(expense)-1; i++ {
+		for j := i + 1; j < len(expense); j++ {
+			if (expense[i] + expense[j]) == 2020 {
+				log.Printf("Pairs (%d %d) - p1 answers is %d", expense[i], expense[j], expense[i]*expense[j])
+			}
+		}
+	}
+
+}
+
+func getExpense(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+	var expense []int
+	for scanner.Scan() {
+		if v, err := strconv.Atoi(scanner.Text()); err != nil {
+			return nil, err
+		} else {
+			expense = append(expense, v)
+		}
+	}
+	return expense, scanner.Err()
+}
